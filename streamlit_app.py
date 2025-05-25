69d37f1bfc35a5831f7f68235ef969635fa2738e
balu337 <165375993+balu337@users.noreply.github.com>
2025-05-25 14:26:19 +0000

    first change

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 My Streamlit app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "XXX Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
